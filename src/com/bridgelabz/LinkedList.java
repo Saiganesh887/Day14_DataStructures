e7fbdaa4eeab21f367bbe91bd4a9adcbbba10e05
Your Name <you@example.com>
2023-01-18 10:47:08 +0530

    Linked list

---
+++ src/com/bridgelabz/LinkedList.java
@@ -0,0 +1,24 @@
+package com.bridgelabz;
+
+public class LinkedList <G>{
+    Node<G> head;
+    Node<G> tail;
+    public void push(G data) {
+        Node<G> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+    public void display() {
+        Node<G> temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+}
---
+++ src/com/bridgelabz/Node.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class Node <G>{
+    G data;
+    Node<G> next;
+    public Node(G data){
+        this.data = data;
+    }
+}
